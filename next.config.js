54f7bf3ca2365cba9c27cdb331fefeea89732ea8
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-07-27 17:03:41 +0300

    Simple dot rewrite

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/./hello",
+        destination: "/api/hello",
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
